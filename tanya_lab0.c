6cf2f7e47d39bbaed0cf768bf4d670acaf7a15f0
myoryo <gawryomyo@gmail.com>
2020-09-26 15:16:03 +0700

    Update tanya_lab0.c

--- tanya_lab0.c
+++ tanya_lab0.c
@@ -10,16 +10,19 @@ int main(int argc, char* argv[])
 	pid_t pid = fork();
 
 	if ( pid == 0 ) {
-		execvp(argv[1], argv);
+		execvp(argv[1], &argv[2]);
 	}
-
-	int status;
-
-	waitpid(pid, &status, 0);
-
-	if ( WIFEXITED(status) ) {
-		int exit_status = WEXITSTATUS(status);
-		printf("Exit status of the child was %d\n", exit_status);
+	if(pid ==-1){
+		printf("Process was not created");
+		exit(-1);	
+	}
+	if (pid>0){
+		int status;
+		waitpid(pid, &status, 0);
+		if ( WIFEXITED(status) ) {
+			int exit_status = WEXITSTATUS(status);
+			printf("Exit status of the child was %d\n", exit_status);
+		}
 	}
 	return 0;
 } 
