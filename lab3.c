fc0925a2268c35eed7620edd9238541c7e352bb2
myoryo <gawryomyo@gmail.com>
2020-10-30 16:43:22 +0700

    Add files via upload

--- lab3.c
+++ lab3.c
@@ -23,22 +23,38 @@ int main(int argc, char *argv[]) {
             tp="d";
         } 
         else tp="?"; 
-    
-        char *access; //доступ
-        if(buff.st_mode & S_IRUSR){ 
-            access="r";
-        }
-        else if(buff.st_mode & S_IWUSR){
-            access="w";
-        }
-        else if(buff.st_mode & S_IXUSR){
-            access="x";
-        }
-        else{
+        int flag=0;
+        char userAccess[3] = {'-', '-', '-'};
+        if (S_IRUSR & buff.st_mode)
+            userAccess[0] = 'r';
+        else flag=1;
+        if (S_IWUSR & buff.st_mode)
+            userAccess[1] = 'w';
+        if (S_IXUSR & buff.st_mode)
+            userAccess[2] = 'x';
+
+        char groupAccess[3] = {'-', '-', '-'};
+        if (S_IRGRP & buff.st_mode)
+            groupAccess[0] = 'r';
+        if (S_IWGRP & buff.st_mode)
+            groupAccess[1] = 'w';
+        if (S_IXGRP & buff.st_mode)
+            groupAccess[2] = 'x';
+
+        char othersAccess[3] = {'-', '-', '-'};
+        if (S_IROTH & buff.st_mode)
+            othersAccess[0] = 'r';
+        if (S_IWOTH & buff.st_mode)
+            othersAccess[1] = 'w';
+        if (S_IXOTH & buff.st_mode)
+            othersAccess[2] = 'x';
+        char *access;
+        if(flag==1){
             struct passwd *pw = getpwuid(buff.st_uid);
             struct group  *gr = getgrgid(buff.st_gid);
             access=strcat(pw->pw_name," ");
             access=strcat(access,gr->gr_name);
+            
         }
         time_t lt; //последняя модификация
         lt = buff.st_mtime;
@@ -54,7 +70,11 @@ int main(int argc, char *argv[]) {
             str=strtok(NULL,pars);
         } while (str!=NULL);
         n++;
-        printf("| %s | %10s | %10s | %10s | %s\n",tp, access, size, name, time );
+        if(flag==1)
+            printf("| %s |%20s| %10s | %15s | %s\n",tp, access, size, name, time );
+        else printf("| %s | %10c%c%c %c%c%c %c%c%c | %10s | %15s | %s\n",tp, userAccess[0], userAccess[1], userAccess[2],
+                groupAccess[0], groupAccess[1], groupAccess[2],
+                othersAccess[0], othersAccess[1], othersAccess[2], size, name, time );
     }
    
 }
